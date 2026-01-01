7caa421b072ee773d9e43bc4c33490b34b713eaa
Prasanth-P29 <prasanth477361@gmail.com>
2026-01-01 21:15:30 +0530

    commit

--- src/apiMethods.ts
+++ src/apiMethods.ts
@@ -1,19 +1,19 @@
-// import { APIRequestContext } from "@playwright/test";
-// import { baseUrl } from "./apiClient";
-// import { Endpoints } from "./enums";
+import { APIRequestContext } from "@playwright/test";
+import { baseUrl } from "./apiClient";
+import { Endpoints } from "./enums";
 
-// export const createUser = async (request: APIRequestContext, data: any) => {
-//   return await request.post(`${baseUrl}${Endpoints.USERS}`, { data });
-// };
+export const createUser = async (request: APIRequestContext, data: any) => {
+  return await request.post(`${baseUrl}${Endpoints.USERS}`, { data });
+};
 
-// export const getUser = async (request: APIRequestContext, id: string) => {
-//   return await request.get(`${baseUrl}${Endpoints.USERS}/${id}`);
-// };
+export const getUser = async (request: APIRequestContext, id: string) => {
+  return await request.get(`${baseUrl}${Endpoints.USERS}/${id}`);
+};
 
-// export const updateUser = async (request: APIRequestContext, id: string, data: any) => {
-//   return await request.put(`${baseUrl}${Endpoints.USERS}/${id}`, { data });
-// };
+export const updateUser = async (request: APIRequestContext, id: string, data: any) => {
+  return await request.put(`${baseUrl}${Endpoints.USERS}/${id}`, { data });
+};
 
-// export const deleteUser = async (request: APIRequestContext, id: string) => {
-//   return await request.delete(`${baseUrl}${Endpoints.USERS}/${id}`);
-// };
+export const deleteUser = async (request: APIRequestContext, id: string) => {
+  return await request.delete(`${baseUrl}${Endpoints.USERS}/${id}`);
+};
