2020de8c965eb741663f03197155862f22429440
Prasanth-P29 <prasanth477361@gmail.com>
2026-01-04 22:08:38 +0530

    commit

--- src/enums.ts
+++ src/enums.ts
@@ -1,15 +1,15 @@
-export enum Endpoints {
-  // Base URL includes the /api/{key} portion. Resource path should be the resource name.
-  USERS = "/users", // Full URL -> {BASE_URL}/users
-}
+// export enum Endpoints {
+//   // Base URL includes the /api/{key} portion. Resource path should be the resource name.
+//   USERS = "/users", // Full URL -> {BASE_URL}/users
+// }
 
-export enum StatusCodes {
-  OK = 200,
-  CREATED = 201,
+// export enum StatusCodes {
+//   OK = 200,
+//   CREATED = 201,
   
-  UPDATED = 200, // returns 200 OK for a successful PUT/PATCH operation
+//   UPDATED = 200, // returns 200 OK for a successful PUT/PATCH operation
   
-  DELETED = 204, // returns 204 No Content for a successful DELETE operation
-  NO_CONTENT = 204, // Alias for clarity
-  BAD_REQUEST = 400,
-}
+//   DELETED = 204, // returns 204 No Content for a successful DELETE operation
+//   NO_CONTENT = 204, // Alias for clarity
+//   BAD_REQUEST = 400,
+// }
